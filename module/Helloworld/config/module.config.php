f99dbbeebc722f2b429c5d9f6ed06beb307a43e9
Gerton Sluis <info@sluisitconsultancy.nl>
2014-10-30 11:15:22 +0100

    added albums and layout changes

--- module/Helloworld/config/module.config.php
+++ module/Helloworld/config/module.config.php
@@ -1,10 +1,21 @@
 <?php
 return array(
-   	'view_manager' => array(
-       	'template_path_stack' => array(
-           	__DIR__ . '/../view'
-       	)
-   	),
+    'display_not_found_reason' => true,
+    'display_exceptions'       => true,
+    'doctype'                  => 'HTML5',
+    'not_found_template'       => 'error/404',
+    'exception_template'       => 'error/index',
+    'template_map' => array(
+        'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+        'helloworld/index/index' => __DIR__ . '/../view/helloworld/index/index.phtml',
+        'error/404'               => __DIR__ . '/../view/error/404.phtml',
+        'error/index'             => __DIR__ . '/../view/error/index.phtml',
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view'
+        )
+    ),
     'router' => array(
         'routes' => array(
             'sayhello' => array(
